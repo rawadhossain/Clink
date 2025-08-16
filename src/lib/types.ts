b94ee9d4467643509c0dde86de04d49176902e8f
Rawad Hossain <rawad.hossain00@gmail.com>
2025-08-16 21:31:06 +0600

    types file added

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,3 +2,12 @@ export interface CreateNoteRequest {
 	title: string;
 	content: string;
 }
+
+export interface Note {
+	id: string;
+	title: string;
+	content: string;
+	ownerId: string;
+	createdAt: string;
+	updatedAt: string;
+}
